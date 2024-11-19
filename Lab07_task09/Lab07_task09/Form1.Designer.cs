0ca0d23f5552c96d11ffc8948f3521d8dcf2223d
Komal514 <233520@students.au.edu.pk>
2024-11-19 05:29:33 -0800

    Add files via upload

---
+++ Lab07_task09/Lab07_task09/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Lab07_task09
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblRemainingChars = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(100, 33);
+            this.txtInput.MaxLength = 160;
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(273, 178);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // lblRemainingChars
+            // 
+            this.lblRemainingChars.AutoSize = true;
+            this.lblRemainingChars.Location = new System.Drawing.Point(118, 279);
+            this.lblRemainingChars.Name = "lblRemainingChars";
+            this.lblRemainingChars.Size = new System.Drawing.Size(199, 20);
+            this.lblRemainingChars.TabIndex = 1;
+            this.lblRemainingChars.Text = "Remaining characters: 160";
+            this.lblRemainingChars.Click += new System.EventHandler(this.lblRemainingChars_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblRemainingChars);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblRemainingChars;
+    }
+}
+
---
+++ Lab07_task09/Lab07_task09/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab07_task09
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Set the MaxLength
+            txtInput.MaxLength = 160;
+            // Initialize  the max length
+            lblRemainingChars.Text = $"Remaining characters: {txtInput.MaxLength}";
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            // calculate remaining characters
+            int remaining = txtInput.MaxLength - txtInput.Text.Length;
+
+            // Update remaining characters
+            lblRemainingChars.Text = $"Remaining characters: {remaining}";
+        }
+
+        private void lblRemainingChars_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
